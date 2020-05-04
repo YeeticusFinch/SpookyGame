44679797267e0df64ef15a1d79130225596eb050
YeeticusFinch <50182007+YeeticusFinch@users.noreply.github.com>
2020-05-04 12:55:39 -0700

    yay its library time

---
+++ SpookyGameClient/src/okGraphics.java
@@ -0,0 +1,17 @@
+import processing.core.PApplet;
+
+public class okGraphics extends PApplet {
+	
+	public okGraphics() {
+		
+	}
+	
+	public void setup() {
+		
+	}
+	
+	public void draw() {
+		
+	}
+	
+}
